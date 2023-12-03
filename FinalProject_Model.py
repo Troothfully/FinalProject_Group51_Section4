32d35239000b1e8083bfa4a2037ebf11a46d5a1c
Troothfully <144928127+Troothfully@users.noreply.github.com>
2023-12-03 09:17:31 -0500

    Modified if statement to properly use file_name rather than file

--- FinalProject_Model.py
+++ FinalProject_Model.py
@@ -1,7 +1,7 @@
 from pydub import AudioSegment
 
 def wavConversion(file_name):
-  if file.path.endswith('.mp3') or file.path.endswith('flac'):
+  if file_name.path.endswith('.mp3') or file_name.path.endswith('flac'):
     file = AudioSegment.from_file(file_name)
     new_file_name = file_name.split('.')[0] + ".wav"
     file.export(new_file_name, format='wav')
