ec64616efe78f64b592e7c9fc6b15f1ee09eaea1
Troothfully <144928127+Troothfully@users.noreply.github.com>
2023-12-04 10:31:37 -0500

    Code to convert file type AND save file name

--- FinalProject_Model.py
+++ FinalProject_Model.py
@@ -1,8 +1,28 @@
+import re
 from pydub import AudioSegment
 
-def wavConversion(file_name):
-  if file_name.path.endswith('.mp3') or file_name.path.endswith('flac'):
-    file = AudioSegment.from_file(file_name)
-    new_file_name = file_name.split('.')[0] + ".wav"
-    file.export(new_file_name, format='wav')
+class FileInput:
+    
+    def __init__(self, file_name):
+        self.file_name = file_name
 
+    @property
+    def file(self):
+        return self.file_name
+    
+    @file.setter
+    def file(self, file_name):
+        pattern = r'\b[A-Za-z0-9._%+-]+.wav\b'
+        if re.fullmatch(pattern, file_name):
+            self.file_name = file_name
+        elif file_name.path.endswith('.mp3') or file_name.path.endswith('.flac'):
+            file = AudioSegment.from_file(file_name)
+            new_file_name = file_name.split('.')[0] + ".wav"
+            file.export(new_file_name, format='wav')
+            self.file_name = new_file_name
+        else:
+            raise ValueError(f'Invalid file type (must be .mp3, .wav, or .flac): {file_name}')
+
+    def save(self):
+        with open('file_name.txt', 'a') as f:
+            f.write(self.file_name + '\n')
