17d4a3fdeb8bfe2a37622242741d96bd4d6a5c50
Troothfully <144928127+Troothfully@users.noreply.github.com>
2023-12-10 15:01:54 -0500

    Final Upload, project functional
    final upload - project functioning.

--- FinalProject_Model.py
+++ FinalProject_Model.py
@@ -1,17 +1,15 @@
-import re
 import numpy as np
 from scipy.io import wavfile
-import scipy.io
 import matplotlib.pyplot as plt
-import re
 from pydub import AudioSegment
 from scipy.signal import find_peaks
 import os
-import librosa
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
 class Model:
     def __init__(self, file_name):
+        self.spectrum = None
+        self.freqs = None
+        self.t = None
+        self.im = None
         self.file_name = file_name
         self.rt60high = None
         self.rt60mid = None
@@ -72,105 +70,223 @@ class Model:
         self.mid_frequency = np.median(np.abs(self.frequencies[peaks]))
         # Find the high frequency
         self.high_frequency = np.max(self.frequencies[peaks])
-        # calculate rt60 values
-        self.rt60low = self.calculate_low_freq_rt60()
-        self.rt60mid = self.calculate_mid_freq_rt60()
-        self.rt60high = self.calculate_high_freq_rt60()
-
-    def calculate_low_freq_rt60(self, low_freq_cutoff=250):
-        # Load the audio file
-        y, sr = librosa.load(self.file_name, sr=None)
-        # Calculate the STFT (Short-Time Fourier Transform)
-        stft = librosa.stft(y)
-        # Calculate the power spectrogram
-        power_spectrogram = np.abs(stft) ** 2
-        # Sum the power spectrogram across frequency bins
-        power_sum = np.sum(power_spectrogram, axis=0)
-        # Calculate the cumulative energy
-        cumulative_energy = np.cumsum(power_sum) / np.sum(power_sum)
-        # Find the index where the cumulative energy exceeds 60%
-        rt60_index = np.argmax(cumulative_energy >= 0.6)
-        # Convert the index to frequency in Hz
-        rt60_freq = librosa.fft_frequencies(sr=sr, n_fft=stft.shape[0])[rt60_index]
-        # Calculate RT60 using the formula: RT60 = 0.049 * N / (f_hi - f_lo)
-        rt60 = 0.049 * stft.shape[1] / (rt60_freq - low_freq_cutoff)
-        return rt60
-
-    def calculate_mid_freq_rt60(self, low_freq_cutoff=250, high_freq_cutoff=5000):
-        # Load the audio file
-        y, sr = librosa.load(self.file_name, sr=None)
-        # Calculate the STFT (Short-Time Fourier Transform)
-        stft = librosa.stft(y)
-        # Calculate the power spectrogram
-        power_spectrogram = np.abs(stft) ** 2
-        # Define frequency bins
-        frequencies = librosa.fft_frequencies(sr=sr, n_fft=stft.shape[0])
-        # Find the indices corresponding to the specified frequency range
-        low_freq_index = np.argmax(frequencies >= low_freq_cutoff)
-        high_freq_index = np.argmax(frequencies >= high_freq_cutoff)
-        # Sum the power spectrogram in the specified frequency range
-        power_sum = np.sum(power_spectrogram[:, low_freq_index:high_freq_index + 1], axis=1)
-        # Calculate the cumulative energy
-        cumulative_energy = np.cumsum(power_sum) / np.sum(power_sum)
-        # Find the index where the cumulative energy exceeds 60%
-        rt60_index = np.argmax(cumulative_energy >= 0.6)
-        # Convert the index to frequency in Hz
-        rt60_freq = frequencies[low_freq_index + rt60_index]
-        # Calculate RT60 using the formula: RT60 = 0.049 * N / (f_hi - f_lo)
-        rt60 = 0.049 * stft.shape[1] / (rt60_freq - low_freq_cutoff)
-        return rt60
-
-    def calculate_high_freq_rt60(self, high_freq_cutoff=5000):
-        # Load the audio file
-        y, sr = librosa.load(self.file_name, sr=None)
-        # Calculate the STFT (Short-Time Fourier Transform)
-        stft = librosa.stft(y)
-        # Calculate the power spectrogram
-        power_spectrogram = np.abs(stft) ** 2
-        # Define frequency bins
-        frequencies = librosa.fft_frequencies(sr=sr, n_fft=stft.shape[0])
-        # Find the index corresponding to the specified high frequency cutoff
-        high_freq_index = np.argmax(frequencies >= high_freq_cutoff)
-        # Sum the power spectrogram in the high-frequency range
-        power_sum = np.sum(power_spectrogram[:, high_freq_index:], axis=1)
-        # Calculate the cumulative energy
-        cumulative_energy = np.cumsum(power_sum) / np.sum(power_sum)
-        # Find the index where the cumulative energy exceeds 60%
-        rt60_index = np.argmax(cumulative_energy >= 0.6)
-        # Convert the index to frequency in Hz
-        rt60_freq = frequencies[high_freq_index + rt60_index]
-        # Calculate RT60 using the formula: RT60 = 0.049 * N / (f_hi - f_lo)
-        rt60 = 0.049 * stft.shape[1] / (rt60_freq - high_freq_cutoff)
-        return rt60
+        self.spectrum, self.freqs, self.t, self.im = plt.specgram(self.data, Fs=self.sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
 
         # function to plot waveform of the FFT
     def plotFFT(self):
         fig = plt.Figure(figsize=(5, 4), dpi=100)
-        fig.legend(['Channel 1'])
         fig.suptitle('FFT Waveform')
         fig.supxlabel('Frequency (Hz)')
         fig.supylabel('Amplitude')
-        fig.add_subplot(111).plot(self.frequencies, self.fft_result)
+        ax = fig.add_subplot(111).plot(self.frequencies, self.fft_result)
+        fig.legend(ax, ['Channel 1'])
         return fig
 
         # Function to plot spectrogram (one additional plot assigned)
+
     def plotSpectrogram(self):
         fig = plt.Figure(figsize=(5, 4), dpi=100)
-        sample_rate, data = wavfile.read(self.file_name)
         fig.suptitle('Spectrogram')
-        spectrum, freqs, t, im = plt.specgram(data, Fs=sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
-        fig.colorbar(im)
         fig.supxlabel('Time (s)')
         fig.supylabel('Frequency (Hz)')
-        #fig.cbar.set_label('Intensity (dB)')
-        fig.add_subplot(111).specgram(data, Fs=sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
+        ax = fig.add_subplot(111).specgram(self.data, Fs=self.sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
+        cbar = fig.colorbar(self.im)
+        cbar.set_label('Intensity (dB)')
         return fig
 
+    #waveform graph
     def plotTimeAmp(self):
         fig = plt.Figure(figsize=(5, 4), dpi=100)
-        fig.legend(['Channel 1'])
         fig.suptitle('Time/Amplitude Waveform')
         fig.supxlabel('Time(s)')
         fig.supylabel('Amplitude')
-        fig.add_subplot(111).plot(self.time, self.data)
+        ax = fig.add_subplot(111).plot(self.time, self.data)
+        fig.legend(ax, ['Channel 1'])
         return fig
+
+    #rt60 mid graph
+    def plotRT60mid(self):
+        data_in_db_mid = self.frequency_check_mid()
+        fig = plt.Figure(figsize=(5, 4), dpi=100)
+        fig.suptitle('RT60 Mid Frequency')
+        fig.supxlabel('Time (s)')
+        fig.supylabel('Power (dB)')
+        index_of_max_mid = np.argmax(data_in_db_mid)
+        value_of_max_mid = data_in_db_mid[index_of_max_mid]
+        sliced_array_mid = data_in_db_mid[index_of_max_mid:]
+        value_of_max_less_5_mid = value_of_max_mid - 5
+        value_of_max_less_5_mid = self.find_nearest_value(sliced_array_mid, value_of_max_less_5_mid)
+        index_of_max_less_5_mid = np.where(data_in_db_mid == value_of_max_less_5_mid)
+        value_of_max_less_25_mid = value_of_max_mid - 25
+        value_of_max_less_25_mid = self.find_nearest_value(sliced_array_mid,value_of_max_less_25_mid)
+        index_of_max_less_25_mid = np.where(data_in_db_mid == value_of_max_less_25_mid)
+        rt20 = (self.t[index_of_max_less_5_mid]-self.t[index_of_max_less_25_mid])[0]
+        rt60 = 3 * rt20
+        self.rt60mid = round(abs(rt60),2)
+        ax = fig.add_subplot(111).plot(self.t, data_in_db_mid, '-m',
+                                  self.t[index_of_max_mid], data_in_db_mid[index_of_max_mid], 'go',
+                                  self.t[index_of_max_less_5_mid], data_in_db_mid[index_of_max_less_5_mid], 'yo',
+                                  self.t[index_of_max_less_25_mid], data_in_db_mid[index_of_max_less_25_mid], 'ro',
+                                  linewidth=1)
+        fig.legend(ax, ['Mid Frequency'])
+        return fig
+
+    def plotRT60low(self):
+        data_in_db_low = self.frequency_check_low()
+        fig = plt.Figure(figsize=(5, 4), dpi=100)
+        fig.suptitle('RT60 Low Frequency')
+        fig.supxlabel('Time (s)')
+        fig.supylabel('Power (dB)')
+        index_of_max_low = np.argmax(data_in_db_low)
+        value_of_max_low = data_in_db_low[index_of_max_low]
+        sliced_array_low = data_in_db_low[index_of_max_low:]
+        value_of_max_less_5_low = value_of_max_low - 5
+        value_of_max_less_5_low = self.find_nearest_value(sliced_array_low, value_of_max_less_5_low)
+        index_of_max_less_5_low = np.where(data_in_db_low == value_of_max_less_5_low)
+        value_of_max_less_25_low = value_of_max_low - 25
+        value_of_max_less_25_low = self.find_nearest_value(sliced_array_low,value_of_max_less_25_low)
+        index_of_max_less_25_low = np.where(data_in_db_low == value_of_max_less_25_low)
+        rt20 = (self.t[index_of_max_less_5_low]-self.t[index_of_max_less_25_low])[0]
+        rt60 = 3 * rt20
+        self.rt60low = round(abs(rt60),2)
+        ax = fig.add_subplot(111).plot(self.t, data_in_db_low, '-c',
+                                  self.t[index_of_max_low], data_in_db_low[index_of_max_low], 'go',
+                                  self.t[index_of_max_less_5_low], data_in_db_low[index_of_max_less_5_low], 'yo',
+                                  self.t[index_of_max_less_25_low], data_in_db_low[index_of_max_less_25_low], 'ro',
+                                  linewidth=1)
+        fig.legend(ax, ['Low Frequency'])
+        return fig
+
+    def plotRT60high(self):
+        data_in_db_high = self.frequency_check_high()
+        fig = plt.Figure(figsize=(5, 4), dpi=100)
+        fig.suptitle('RT60 High Frequency')
+        fig.supxlabel('Time (s)')
+        fig.supylabel('Power (dB)')
+        index_of_max_high = np.argmax(data_in_db_high)
+        value_of_max_high = data_in_db_high[index_of_max_high]
+        sliced_array_high = data_in_db_high[index_of_max_high:]
+        value_of_max_less_5_high = value_of_max_high - 5
+        value_of_max_less_5_high = self.find_nearest_value(sliced_array_high, value_of_max_less_5_high)
+        index_of_max_less_5_high = np.where(data_in_db_high == value_of_max_less_5_high)
+        value_of_max_less_25_high = value_of_max_high - 25
+        value_of_max_less_25_high = self.find_nearest_value(sliced_array_high,value_of_max_less_25_high)
+        index_of_max_less_25_high = np.where(data_in_db_high == value_of_max_less_25_high)
+        rt20 = (self.t[index_of_max_less_5_high]-self.t[index_of_max_less_25_high])[0]
+        rt60 = 3 * rt20
+        self.rt60high = round(abs(rt60),2)
+        ax = fig.add_subplot(111).plot(self.t, data_in_db_high, '-k',
+                                  self.t[index_of_max_high], data_in_db_high[index_of_max_high], 'go',
+                                  self.t[index_of_max_less_5_high], data_in_db_high[index_of_max_less_5_high], 'yo',
+                                  self.t[index_of_max_less_25_high], data_in_db_high[index_of_max_less_25_high], 'ro',
+                                  linewidth=1)
+        fig.legend(ax, ['High Frequency'])
+        return fig
+
+    def plotRT60Combined(self):
+        #figure setup
+        fig = plt.Figure(figsize=(5, 4), dpi=100)
+        fig.suptitle('RT60 Combined')
+        fig.supxlabel('Time (s)')
+        fig.supylabel('Power (dB)')
+        #data for low
+        data_in_db_low = self.frequency_check_low()
+        index_of_max_low = np.argmax(data_in_db_low)
+        value_of_max_low = data_in_db_low[index_of_max_low]
+        sliced_array_low = data_in_db_low[index_of_max_low:]
+        value_of_max_less_5_low = value_of_max_low - 5
+        value_of_max_less_5_low = self.find_nearest_value(sliced_array_low, value_of_max_less_5_low)
+        index_of_max_less_5_low = np.where(data_in_db_low == value_of_max_less_5_low)
+        value_of_max_less_25_low = value_of_max_low - 25
+        value_of_max_less_25_low = self.find_nearest_value(sliced_array_low, value_of_max_less_25_low)
+        index_of_max_less_25_low = np.where(data_in_db_low == value_of_max_less_25_low)
+        #data for mid
+        data_in_db_mid = self.frequency_check_mid()
+        index_of_max_mid = np.argmax(data_in_db_mid)
+        value_of_max_mid = data_in_db_mid[index_of_max_mid]
+        sliced_array_mid = data_in_db_mid[index_of_max_mid:]
+        value_of_max_less_5_mid = value_of_max_mid - 5
+        value_of_max_less_5_mid = self.find_nearest_value(sliced_array_mid, value_of_max_less_5_mid)
+        index_of_max_less_5_mid = np.where(data_in_db_mid == value_of_max_less_5_mid)
+        value_of_max_less_25_mid = value_of_max_mid - 25
+        value_of_max_less_25_mid = self.find_nearest_value(sliced_array_mid, value_of_max_less_25_mid)
+        index_of_max_less_25_mid = np.where(data_in_db_mid == value_of_max_less_25_mid)
+        #data for high
+        data_in_db_high = self.frequency_check_high()
+        index_of_max_high = np.argmax(data_in_db_high)
+        value_of_max_high = data_in_db_high[index_of_max_high]
+        sliced_array_high = data_in_db_high[index_of_max_high:]
+        value_of_max_less_5_high = value_of_max_high - 5
+        value_of_max_less_5_high = self.find_nearest_value(sliced_array_high, value_of_max_less_5_high)
+        index_of_max_less_5_high = np.where(data_in_db_high == value_of_max_less_5_high)
+        value_of_max_less_25_high = value_of_max_high - 25
+        value_of_max_less_25_high = self.find_nearest_value(sliced_array_high, value_of_max_less_25_high)
+        index_of_max_less_25_high = np.where(data_in_db_high == value_of_max_less_25_high)
+        #add to fig
+        ax = fig.add_subplot(111).plot(self.t, data_in_db_low, '-c',
+                                  self.t, data_in_db_mid, '-m',
+                                  self.t, data_in_db_high, '-k',
+                                  #dots for low
+                                  self.t[index_of_max_low], data_in_db_low[index_of_max_low], 'go',
+                                  self.t[index_of_max_less_5_low], data_in_db_low[index_of_max_less_5_low], 'yo',
+                                  self.t[index_of_max_less_25_low], data_in_db_low[index_of_max_less_25_low], 'ro',
+                                  #dots for mid
+                                  self.t[index_of_max_mid], data_in_db_mid[index_of_max_mid], 'go',
+                                  self.t[index_of_max_less_5_mid], data_in_db_mid[index_of_max_less_5_mid], 'yo',
+                                  self.t[index_of_max_less_25_mid], data_in_db_mid[index_of_max_less_25_mid], 'ro',
+                                  #dots for high
+                                  self.t[index_of_max_high], data_in_db_high[index_of_max_high], 'go',
+                                  self.t[index_of_max_less_5_high], data_in_db_high[index_of_max_less_5_high], 'yo',
+                                  self.t[index_of_max_less_25_high], data_in_db_high[index_of_max_less_25_high], 'ro',
+                                  linewidth = 1)
+        fig.legend(ax,['Low Frequency', 'Medium Frequency', 'High Frequency'])
+        return fig
+
+    def find_low_frequency(self, freqs):
+        for x in freqs:
+            if x > self.low_frequency:
+                break
+        return x
+
+    def find_high_frequency(self, freqs):
+        for x in freqs:
+            if x > self.high_frequency:
+                break
+        return x
+
+    def find_mid_frequency(self, freqs):
+        for x in freqs:
+            if x > self.mid_frequency:
+                break
+        return x
+
+
+    def frequency_check_mid(self):
+        target_frequency = self.find_mid_frequency(self.freqs)
+        index_of_frequency = np.where(self.freqs == target_frequency)[0][0]
+        data_for_frequency = self.spectrum[index_of_frequency]
+        data_in_db = 10*np.log10(data_for_frequency)
+        return data_in_db
+
+    def frequency_check_low(self):
+        target_frequency = self.find_low_frequency(self.freqs)
+        index_of_frequency = np.where(self.freqs == target_frequency)[0][0]
+        data_for_frequency = self.spectrum[index_of_frequency]
+        data_in_db = 10*np.log10(data_for_frequency)
+        return data_in_db
+
+    def frequency_check_high(self):
+        target_frequency = self.find_high_frequency(self.freqs)
+        index_of_frequency = np.where(self.freqs == target_frequency)[0][0]
+        data_for_frequency = self.spectrum[index_of_frequency]
+        data_in_db = 10*np.log10(data_for_frequency)
+        return data_in_db
+
+    def find_nearest_value(self,array,value):
+        array = np.asarray(array)
+        idx = (np.abs(array-value)).argmin()
+        return array[idx]
+
+
+
+
