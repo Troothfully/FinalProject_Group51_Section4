3e2ca2486a9bc5c28e478e94e0bcade4e793fc1b
Troothfully <144928127+Troothfully@users.noreply.github.com>
2023-12-09 23:45:13 -0500

    Update FinalProject_Model.py
    Updated code for model file

--- FinalProject_Model.py
+++ FinalProject_Model.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 from scipy.io import wavfile
 import scipy.io
@@ -7,10 +8,11 @@ from pydub import AudioSegment
 from scipy.signal import find_peaks
 import os
 import librosa
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-class File:
-    # Initializes the file object and converts .mp3 to .wav if necessary
+class Model:
     def __init__(self, file_name):
+        self.file_name = file_name
         self.rt60high = None
         self.rt60mid = None
         self.high_frequency = None
@@ -25,120 +27,55 @@ class File:
         self.data = None
         self.sample_rate = None
         self.rt60low = None
-        self.file_name = file_name
 
-    # Getter function for file_name
+# use property decorator to use getters & setters
+# define file an attribute of Model
     @property
     def file(self):
-        return self.file_name
+        return self.file
 
-    # setter function for file_name
-    def fileSet(self, file_name):
+    @file.setter
+    def file(self, value):
+        if '.wav' in value.lower():
+            self.file_name = value
 
-        def contains_mp3(file_name):
-            return ".mp3" in file_name.lower()
-
-        def contains_wav(file_name):
-            return ".wav" in file_name.lower()
-
-        if contains_mp3(file_name):
-            wav_file = os.path.splitext(file_name)[0] + '.wav'
-            sound = AudioSegment.from_mp3(file_name)
-            sound.export(wav_file, format="wav")
+        elif '.mp3' in value.lower():
+            wav_file = os.path.splitext(value)[0] + '.wav'
+            sound = AudioSegment.from_mp3(value)
+            sound.export(wav_file, format='wav')
             self.file_name = wav_file
-            # Merges Audio Channels
-            self.mergeChannels()
-            # Defines variables for Audio Properties
-            self.sample_rate, self.data = wavfile.read(self.file_name)
-            self.length = self.data.shape[0] / self.sample_rate
-            self.time = np.linspace(0., self.length, self.data.shape[0])
-            # Perform FFT on the audio data
-            self.fft_result = abs(np.fft.fft(self.data))
-            self.frequencies = abs(np.fft.fftfreq(len(self.fft_result), d=1 / self.sample_rate))
-            self.fft_min = np.max(self.fft_result) * .1
-            # Find peaks in the frequency domain
-            peaks, _ = find_peaks(np.abs(self.fft_result), self.fft_min)
-            # Find the highest peak (resonance frequency)
-            highest_peak_index = np.argmax(np.abs(self.fft_result[peaks]))
-            self.resonance_frequency = self.frequencies[peaks[highest_peak_index]]
-            # Find the low frequency
-            self.low_frequency = np.min(self.frequencies[peaks])
-            # Find the mid frequency
-            self.mid_frequency = np.median(np.abs(self.frequencies[peaks]))
-            # Find the high frequency
-            self.high_frequency = np.max(self.frequencies[peaks])
-            # calculate rt60 values
-            self.rt60low = self.calculate_low_freq_rt60()
-            self.rt60mid = self.calculate_mid_freq_rt60()
-            self.rt60high = self.calculate_high_freq_rt60()
 
-
-        elif contains_wav(file_name):
-            self.file_name = file_name
-            # Merges Audio Channels
-            self.mergeChannels()
-            # Defines variables for Audio Properties
-            self.sample_rate, self.data = wavfile.read(self.file_name)
-            self.length = self.data.shape[0] / self.sample_rate
-            self.time = np.linspace(0., self.length, self.data.shape[0])
-            # Perform FFT on the audio data
-            self.fft_result = abs(np.fft.fft(self.data))
-            self.frequencies = abs(np.fft.fftfreq(len(self.fft_result), d=1 / self.sample_rate))
-            self.fft_min = np.max(self.fft_result) * .1
-            # Find peaks in the frequency domain
-            peaks, _ = find_peaks(np.abs(self.fft_result), self.fft_min)
-            # Find the highest peak (resonance frequency)
-            highest_peak_index = np.argmax(np.abs(self.fft_result[peaks]))
-            self.resonance_frequency = self.frequencies[peaks[highest_peak_index]]
-            # Find the low frequency
-            self.low_frequency = np.min(self.frequencies[peaks])
-            # Find the mid frequency
-            self.mid_frequency = np.median(np.abs(self.frequencies[peaks]))
-            # Find the high frequency
-            self.high_frequency = np.max(self.frequencies[peaks])
-            #calculate rt60 values
-            self.rt60low = self.calculate_low_freq_rt60()
-            self.rt60mid = self.calculate_mid_freq_rt60()
-            self.rt60high = self.calculate_high_freq_rt60()
         else:
-            print(f"{file_name} is neither a MP3 nor a WAV file.")
-            exit()
+            raise ValueError(f'{value} is neither a MP3 nor a WAV file)')
 
-    # function to merge audio channels
-    def mergeChannels(self):
+    def laf(self):
+        # Merges Audio Channels
         sound = AudioSegment.from_wav(self.file_name)
         sound = sound.set_channels(1)
         sound.export('mono_channel.wav', format="wav")
         self.file_name = 'mono_channel.wav'
-
-    # function to plot waveform of .wav file
-    def plotTimeAmp(self):
-        plt.plot(self.time, self.data)
-        plt.legend(['Channel 1'])
-        plt.xlabel('Time (s)')
-        plt.ylabel('Amplitude')
-        plt.title('Time/Amplitude Waveform')
-        plt.show()
-
-    # function to plot waveform of the FFT
-    def plotFFT(self):
-        plt.plot(self.frequencies, self.fft_result)
-        plt.legend(['Channel 1'])
-        plt.xlabel('Frequency (Hz)')
-        plt.ylabel('Amplitude')
-        plt.title('FFT Waveform')
-        plt.show()
-
-    # Function to plot spectrogram (one additional plot assigned)
-    def plotSpectrogram(self):
-        sample_rate, data = wavfile.read(self.file_name)
-        spectrum, freqs, t, im = plt.specgram(data, Fs=sample_rate, \
-                                              NFFT=1024, cmap=plt.get_cmap('autumn_r'))
-        cbar = plt.colorbar(im)
-        plt.xlabel('Time (s)')
-        plt.ylabel('Frequency (Hz)')
-        cbar.set_label('Intensity (dB)')
-        plt.show()
+        self.sample_rate, self.data = wavfile.read(self.file_name)
+        self.length = self.data.shape[0] / self.sample_rate
+        self.time = np.linspace(0., self.length, self.data.shape[0])
+        # Perform FFT on the audio data
+        self.fft_result = abs(np.fft.fft(self.data))
+        self.frequencies = abs(np.fft.fftfreq(len(self.fft_result), d=1 / self.sample_rate))
+        self.fft_min = np.max(self.fft_result) * .1
+        # Find peaks in the frequency domain
+        peaks, _ = find_peaks(np.abs(self.fft_result), self.fft_min)
+        # Find the highest peak (resonance frequency)
+        highest_peak_index = np.argmax(np.abs(self.fft_result[peaks]))
+        self.resonance_frequency = self.frequencies[peaks[highest_peak_index]]
+        # Find the low frequency
+        self.low_frequency = np.min(self.frequencies[peaks])
+        # Find the mid frequency
+        self.mid_frequency = np.median(np.abs(self.frequencies[peaks]))
+        # Find the high frequency
+        self.high_frequency = np.max(self.frequencies[peaks])
+        # calculate rt60 values
+        self.rt60low = self.calculate_low_freq_rt60()
+        self.rt60mid = self.calculate_mid_freq_rt60()
+        self.rt60high = self.calculate_high_freq_rt60()
 
     def calculate_low_freq_rt60(self, low_freq_cutoff=250):
         # Load the audio file
@@ -206,8 +143,34 @@ class File:
         rt60 = 0.049 * stft.shape[1] / (rt60_freq - high_freq_cutoff)
         return rt60
 
-userFile = File('SampleFile')
-userFile.fileSet('sample-3s.mp3')
-userFile.plotTimeAmp()
-userFile.plotFFT()
-userFile.plotSpectrogram()
+        # function to plot waveform of the FFT
+    def plotFFT(self):
+        fig = plt.Figure(figsize=(5, 4), dpi=100)
+        fig.legend(['Channel 1'])
+        fig.suptitle('FFT Waveform')
+        fig.supxlabel('Frequency (Hz)')
+        fig.supylabel('Amplitude')
+        fig.add_subplot(111).plot(self.frequencies, self.fft_result)
+        return fig
+
+        # Function to plot spectrogram (one additional plot assigned)
+    def plotSpectrogram(self):
+        fig = plt.Figure(figsize=(5, 4), dpi=100)
+        sample_rate, data = wavfile.read(self.file_name)
+        fig.suptitle('Spectrogram')
+        spectrum, freqs, t, im = plt.specgram(data, Fs=sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
+        fig.colorbar(im)
+        fig.supxlabel('Time (s)')
+        fig.supylabel('Frequency (Hz)')
+        #fig.cbar.set_label('Intensity (dB)')
+        fig.add_subplot(111).specgram(data, Fs=sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
+        return fig
+
+    def plotTimeAmp(self):
+        fig = plt.Figure(figsize=(5, 4), dpi=100)
+        fig.legend(['Channel 1'])
+        fig.suptitle('Time/Amplitude Waveform')
+        fig.supxlabel('Time(s)')
+        fig.supylabel('Amplitude')
+        fig.add_subplot(111).plot(self.time, self.data)
+        return fig
