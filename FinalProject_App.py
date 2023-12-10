f406b3e60d484e06c295623e0e288b550df8dfab
Troothfully <144928127+Troothfully@users.noreply.github.com>
2023-12-09 23:41:24 -0500

    Update FinalProject_App.py
    Updated code for app

--- FinalProject_App.py
+++ FinalProject_App.py
@@ -1,25 +1,29 @@
-from model import File
+from model import Model
 from view import View
 from controller import Controller
 import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title('COP2080 Final Project')
+
+        self.title('Final Project COP2080')
 
         # create a model
-        userFile = File('sample_file.wav')
+        model = Model('test.wav')
 
         # create a view and place it on the root window
         view = View(self)
         view.grid(row=0, column=0, padx=10, pady=10)
 
         # create a controller
-        controller = Controller(userFile, view)
+        controller = Controller(model, view)
 
         # set the controller to view
         view.set_controller(controller)
 
-        if __name__ == '__main__':
-            app = App()
-            app.mainloop()
+
+if __name__ == '__main__':
+    app = App()
+    app.geometry('525x650')
+    app.mainloop()
